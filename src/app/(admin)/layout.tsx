f9efcc50db75fd8438f60005f7bfffa903ade91f
zaradavid83 <zb7ybr84d8@privaterelay.appleid.com>
2026-09-06 11:41:24 -0400

    fix: logos marca de agua mas grandes y visibles

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -10,13 +10,17 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
         <Sidebar />
         <div className="flex-1 flex flex-col ml-64">
           <Header />
-          <main className="flex-1 p-8 mt-16 relative overflow-hidden">
-            {/* Marcas de agua - logos institucionales */}
-            <div className="pointer-events-none fixed inset-0 ml-64 mt-16 z-0 overflow-hidden">
-              <img src="/logos/alcaldia.jpg" alt="" className="absolute top-6 right-6 h-24 w-auto object-contain opacity-[0.07]" />
-              <img src="/logos/isma.jpg" alt="" className="absolute bottom-6 right-6 h-24 w-auto object-contain opacity-[0.07]" />
-              <img src="/logos/basura_cero.jpg" alt="" className="absolute bottom-6 left-6 h-24 w-auto object-contain opacity-[0.07]" />
-              <img src="/logos/global_rec.jpg" alt="" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-48 w-auto object-contain opacity-[0.05]" />
+          <main className="flex-1 p-8 mt-20 relative min-h-screen">
+            {/* Marcas de agua - logos institucionales en esquinas */}
+            <div className="pointer-events-none fixed inset-0 ml-64 mt-20 z-0">
+              {/* Superior izquierda - Alcaldía */}
+              <img src="/logos/alcaldia.jpg" alt="" className="absolute top-4 left-4 h-28 w-auto object-contain opacity-20" />
+              {/* Superior derecha - ISMA */}
+              <img src="/logos/isma.jpg" alt="" className="absolute top-4 right-4 h-28 w-auto object-contain opacity-20" />
+              {/* Inferior izquierda - Basura Cero */}
+              <img src="/logos/basura_cero.jpg" alt="" className="absolute bottom-4 left-4 h-28 w-auto object-contain opacity-20" />
+              {/* Inferior derecha - Global Rec */}
+              <img src="/logos/global_rec.jpg" alt="" className="absolute bottom-4 right-4 h-28 w-auto object-contain opacity-20" />
             </div>
             <div className="relative z-10">
               {children}
